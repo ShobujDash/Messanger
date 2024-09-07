e5d6d963e142083a6fdf1fc516e8459c3110e932
ShobujDash <158812287+ShobujDash@users.noreply.github.com>
2024-09-07 23:03:52 +0600

    Update index.js
    update cors origin

--- server/index.js
+++ server/index.js
@@ -10,7 +10,9 @@ const {app,server} = require('./socket/index')
 // const app = express();
 app.use(
   cors({
-    origin: process.env.FRNTEND_URL,
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   })
 );
